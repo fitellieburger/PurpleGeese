d7e7fb4c6acee9ac180fdb556af6bd4e2110a581
fitellieburger <116864363+fitellieburger@users.noreply.github.com>
2022-11-17 21:04:06 -1000

    Add files via upload

---
+++ public/js/delete_team.js
@@ -0,0 +1,47 @@
+/*
+    Citation for the code turned in for CS 340 Step 4
+    Date: 11/10/2022
+    Adapted fromNode Starter App
+    Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+*/
+
+function deleteTeam(teamID) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        teamID: teamID
+    };
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/deleteTeamAjax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(teamID);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(teamID){
+
+    let table = document.getElementById("teamTable");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == teamID) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
---
+++ public/js/seasons/delete_season.js
@@ -0,0 +1,47 @@
+/*
+    Citation for the code turned in for CS 340 Step 5
+    Date: 11/16/2022
+    Adapted from Michael McDonell's adaptation of the Node Starter App
+    Node Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+*/
+
+function deleteSeason(seasonID) {
+    // Put our data we want to send in a javascript object
+    let data = {
+        seasonID: seasonID
+    };
+
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/deleteSeason", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+
+            // Add the new data to the table
+            deleteRow(seasonID);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.")
+        }
+    }
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+}
+
+
+function deleteRow(seasonID){
+
+    let table = document.getElementById("seasonTable");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == seasonID) {
+            table.deleteRow(i);
+            break;
+       }
+    }
+}
